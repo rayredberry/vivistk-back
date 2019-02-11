eb0426bd16b75a82cbfff161ca23e9ac6776698d
mac <mac@macs-MacBook-Air-2.local>
2019-02-11 11:28:25 +0400

    continious integration 3

--- app/League.php
+++ app/League.php
@@ -9,7 +9,7 @@ class League extends Model
     //
 
     protected $fillable = [
-        'user_id', 'league_id', 'games', 'win', 'draw', 'lose', 'goals_for', 'goals_against', 'max_score', 'min_score', 'score', 'streak'
+        'name', 'description'
     ];
 
 
